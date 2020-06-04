9d574a4e4fae585f7fd2817805917b67f766544d
shukina7olga <olga.shchukina.99@mail.ru>
2020-06-04 22:23:05 +0300

    Выволнено первое дз

--- script/script.js
+++ script/script.js
@@ -1,5 +1,11 @@
-console.log("Привет гиту");
-console.log("Привет всем!");
-alert('Здравствуйте');
+let money = 1000;
+let income = 40000;
+let addExpenses = 500;
+let deposit = 20000;
+let mission = 'Learn js';
+let period = 1.5;
 
+console.log('money: ', money);
+console.log(mission);
 
+alert(period);
