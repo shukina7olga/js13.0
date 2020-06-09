3d77efed120eafb1f936f3a6e09fd0c9c14aa695
shukina7olga <olga.shchukina.99@mail.ru>
2020-06-09 18:28:00 +0300

    Внесены правки от куратора. Код упрощен: перенесены операции выше к объявленным переменным. Цикл switch работает, так как в скобкахуказала true.

--- script/script.js
+++ script/script.js
@@ -1,69 +1,19 @@
 'use strict';
-let money,
+let money = prompt('Ваш месячный доход'),
     income = 'фриланс',
-    addExpenses,
-    deposit,
+    addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую'),
+    deposit = confirm('Есть ли у вас депозит в банке?'),
     mission = 900000,
     period = 5,
-    budgetDay,
-    expenses1,
-    amount1,
-    expenses2,
-    amount2,
-    budgetMonth,
-    month;
+    expenses1 = prompt('Введите обязательную статью расходов'),
+    amount1 = prompt('Во сколько это ообйдется?'),
+    expenses2 = prompt('Введите обязательную статью расходов'),
+    amount2 = prompt('Во сколько это обойдется?'),
+    budgetMonth = money - (+amount1 + (+amount2)), //пункт_6 вычисляем бюджет на месяц  альтернатива budgetMonth = money - (Number(amount1) + Number(amount2));
+    budgetDay = budgetMonth/30,//пункт_8 деньги на день
+    month = mission/budgetMonth; //пункт_7 за сколько месяцев достигнем цели
 
-//пункт_3_4 спрашиваем данные у пользователя через поле ввода в модалке
-money = prompt('Ваш месячный доход');
-addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
-deposit = confirm('Есть ли у вас депозит в банке?');
-//пункт_5 доп вопросы
-expenses1 = prompt('Введите обязательную статью расходов');
-amount1 = prompt('Во сколько это ообйдется?');
-expenses2 = prompt('Введите обязательную статью расходов');
-amount2 = prompt('Во сколько это обойдется?');
-
-//пункт_6 вычисляем бюджет на месяц
-budgetMonth = money - (+amount1 + (+amount2));
-//альтернатива budgetMonth = money - (Number(amount1) + Number(amount2));
-
-//пункт_7 за сколько месяцев достигнем цели
-month = mission/budgetMonth;
-
-//пункт_8 деньги на день
-budgetDay = budgetMonth/30;
-
-
-
-//из 2 домашки
-console.log(' Месячный доход money: ', typeof money, '\n', 'Дополнительный доход income: ',
-     typeof income, '\n', 'Есть депозит в банке? deposit: ', typeof deposit);
-console.log('Длина введенной строки', addExpenses.length);
-console.log('Период равен:', period, 'месяцев', 'Цель: заработать', mission, 'рублей');
-console.log('Разбили строку на массив из строк', addExpenses.toLowerCase().split(','));
-
-//из 3 домашки
-console.log('Месячный доход:', money, '\n', addExpenses, '\n','Есть депозит в банке? deposit:', deposit, '\n',
-    expenses1, ':', amount1,'\n', expenses2,':', amount2);
-console.log('Бюджет на месяц:', budgetMonth);
-console.log('Месяцев, что бы накопить', mission, 'будет', Math.ceil(month));
-console.log('Бюджет на 1 день:', Math.floor(budgetDay));
-
-//пункт_9 
-if (budgetDay > 1200) {
-    console.log('У вас высокий уровень дохода');
-} else if (1200 >= budgetDay >= 600) {     //budgetDay > 600 && budgetDay < 1200 
-    console.log('У вас средний уровень дохода');
-} else if (budgetDay > 600 || budgetDay === 0) {
-    console.log('К сожалению у вас уровень дохода ниже среднего');
-} else if (budgetDay < 0) {
-    console.log('Что то пошло не так');
-} 
-
-/* Тут ведь нельзя использовать swich из-за того, что в скобках "switch (budgetDay)" 
-    должно присутствовать именно булево значение? Или в скобки просто надо было прописать что-то другое?
-
-switch (budgetDay) { 
+switch (true) { 
     case budgetDay > 1200:
         console.log('У вас высокий уровень дохода');
         break;
@@ -76,4 +26,32 @@ switch (budgetDay) {
     case budgetDay < 0: 
         console.log('Что то пошло не так');
         break;
+}
+/*пункт_9 альтернатива
+if (budgetDay > 1200) {
+    console.log('У вас высокий уровень дохода');
+} else if (1200 >= budgetDay >= 600) {     //budgetDay > 600 && budgetDay < 1200 
+    console.log('У вас средний уровень дохода');
+} else if (budgetDay > 600 || budgetDay === 0) {
+    console.log('К сожалению у вас уровень дохода ниже среднего');
+} else if (budgetDay < 0) {
+    console.log('Что то пошло не так');
 } */
+
+//из 2 домашки
+console.log(' Месячный доход money: ', typeof money, '\n', 'Дополнительный доход income: ',
+     typeof income, '\n', 'Есть депозит в банке? deposit: ', typeof deposit);
+console.log('Длина введенной строки', addExpenses.length);
+console.log('Период равен:', period, 'месяцев', 'Цель: заработать', mission, 'рублей');
+console.log('Разбили строку на массив из строк', addExpenses.toLowerCase().split(','));
+//из 3 домашки
+console.log('Месячный доход:', money, '\n', addExpenses, '\n','Есть депозит в банке? deposit:', deposit, '\n',
+    expenses1, ':', amount1,'\n', expenses2,':', amount2);
+console.log('Бюджет на месяц:', budgetMonth);
+console.log('Месяцев, что бы накопить', mission, 'будет', Math.ceil(month));
+console.log('Бюджет на 1 день:', Math.floor(budgetDay));
+
+
+
+
+
