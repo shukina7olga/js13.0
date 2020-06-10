05d3c7203ae2608dd90873653a5d1613f8518d36
shukina7olga <olga.shchukina.99@mail.ru>
2020-06-11 01:09:07 +0300

    Выполнен 4 доп урок. С методом, удаляющим пробелы с конца и начала

--- script/script.js
+++ script/script.js
@@ -1,44 +1,19 @@
 'use strict';
-let lang = document.getElementsByTagName('html')[0].getAttribute('lang');
+let str;    
+let getString = function(str) {
+    if(typeof str === typeof '') {      
+        str = str.trim(); // метод, который удаляет пробелы с конца и начала
+        if(str.length > 4) {
+            str = str.substr(0, 4) + '...';
+            return str;
+        }
+        return str;   
+    } else {
+        console.log('В качестве аргумента не строка');
+    }
+};
 
-if (lang === "ru") {
-    console.log('понедельник вторник среда четверг пятница суббота воскресение');
-} else {
-    console.log('monday tuesday wednesday thursday friday saturday sunday');
-}
-
-
-switch (lang) {
-    case "ru":
-        console.log('понедельник вторник среда четверг пятница суббота воскресение');
-        break;
-    default:
-        console.log('monday tuesday wednesday thursday friday saturday sunday');
-        break;
-}
-
-
-
-let arr = [['понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота', 'воскресение'],
-            ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']];
-
-let i = 7;
-while (lang === "ru" && i === 7) { 
-  console.log(arr[0][0], arr[0][1], arr[0][2], arr[0][3], arr[0][4], arr[0][5], arr[0][6]);
-  i --; // для выхода из цикла. иначе пипец, будет бесконечно крутить
-}
-
-while (lang === "en" && i === 7) { 
-    console.log(arr[1][0], arr[1][1], arr[1][2], arr[1][3], arr[1][4], arr[1][5], arr[1][6]);
-    i --;
-}
-
-/* альтернатива второму заданию
-let namePerson = prompt('Ты хто?'), n, b, a;
-a = (namePerson === 'Артем') ? console.log('директор') : n ;
-n = (namePerson === 'Максим') ? console.log('преподаватель') : b;
-b = (namePerson !== 'Артем' && namePerson !== 'Максим') ? console.log('студент') : a ; 
-*/
-
-let namePerson = prompt('Ты хто по жизни?');
-(namePerson === 'Артем') ? console.log('директор') : (namePerson === 'Максим') ? console.log('преподаватель') : console.log('студент'); 
+str = getString('     бобрабобрабобрабобрабобрабобрабобра   ');
+//str = getString(7); // но почему-то выводит еще undefined
+console.log(str);
+    
