db414a59fa85dd7f9f962f5fdab7eb0c86d56dd4
shukina7olga <olga.shchukina.99@mail.ru>
2020-06-12 00:09:44 +0300

    Исправленный улучшенный  вариант

--- script/script.js
+++ script/script.js
@@ -1,19 +1,17 @@
 'use strict';
-let str;    
 let getString = function(str) {
-    if(typeof str === typeof '') {      
+    if(typeof str === 'string') {      
         str = str.trim(); // метод, который удаляет пробелы с конца и начала
-        if(str.length > 4) {
-            str = str.substr(0, 4) + '...';
+        if(str.length > 30) {
+            str = str.substr(0, 30) + '...';
             return str;
         }
         return str;   
     } else {
-        console.log('В качестве аргумента не строка');
+        return 'В качестве аргумента не строка';
     }
 };
 
-str = getString('     бобрабобрабобрабобрабобрабобрабобра   ');
-//str = getString(7); // но почему-то выводит еще undefined
-console.log(str);
+console.log(getString('     бобрабобрабобрабобрабобрабобрабобра   '));
+console.log(getString(7));
     
