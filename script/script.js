a9a81d085a81d3eb717c3204e64fc66d387e1ded
shukina7olga <olga.shchukina.99@mail.ru>
2020-06-29 19:22:55 +0300

    14 дз первая часть

--- script/script.js
+++ script/script.js
@@ -8,10 +8,34 @@ function DomElement(selector, height, width, bg, fontSize){
 }
 
 DomElement.prototype.system = function(){ // добавили метод к прототипу объекта
-    console.log('function');
+    
+    if(this.selector[0] === '.'){
+        console.log('див');
+        let div = document.createElement('div');
+        div.className = this.selector.substring(1);
+        div.innerHTML = 'это блок';
+        div.style.height = this.height;
+        div.style.width = this.width;
+        div.style.background = this.bg;
+        div.style.sfontSize = this.fontSize;
+        document.body.append(div);
+    }
+    if(this.selector[0] === '#'){
+        console.log('парагр');
+        let paragr = document.createElement('p');
+        paragr.id = this.selector.substring(1);
+        paragr.innerHTML = 'это параграф';
+        paragr.style.height = this.height;
+        paragr.style.width = this.width;
+        paragr.style.background = this.bg;
+        paragr.style.fontSize = this.fontSize;
+        document.body.append(paragr);
+    }
+    
 };
-let domElement = new DomElement();
+let domElementDiv = new DomElement('.block', '50px', '40px', 'red', '10px');
+let domElementP = new DomElement('#best', '150px', '100px', 'green', '20px');
 
-domElement.system();
-console.log(DomElement);
-console.log(domElement);
+domElementDiv.system();
+domElementP.system();
+
